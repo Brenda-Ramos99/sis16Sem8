c6946686f50f21d4cafca86a1a0598fb944fe574
Stefany-Renderos <49921915+Stefany-Renderos@users.noreply.github.com>
2019-04-24 22:53:17 -0600

    Add files via upload

---
+++ Ejer2/Ejercicio2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $f=0;             
+        
+       for ( $n=1; $n<=100; $n++)
+       {
+           $f=$f+$n;           
+       }
+        
+        echo ("EL total es de: "+$f);
+        ?>
+    </body>
+</html>
+
+
