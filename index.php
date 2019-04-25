221a279bffea72636da42bc78afe3a1e1bd63675
Brenda-Ramos99 <49920605+Brenda-Ramos99@users.noreply.github.com>
2019-04-24 19:52:45 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
-<?php
-echo 'Hola Mundo';
+<!DOCTYPE html>
+<html>
+    <head>
+       <!--ejercicio6--> 
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body> <form action="#" method="POST">
+            <label>Digite un entero positivo :</label>
+            <input type="text" name="num">
+            <input type="submit" name="b1" value="Enviar">
+        </form>
+        <?php
+        if (isset($_POST["b1"])){
+        $num =$_POST["num"];
+        echo '<br><br>';
+        echo "La serie de la conjetura de Ulam es: ";
+        echo "$num";
+        while($num!=1)
+        {
+            if ($num%2==0) 
+            {
+                $num=$num/2;                
+            }
+            else
+            {
+                $num=$num*3+1;
+            }
+            
+            echo ' , ' .$num;
+        }
+        }        
+        ?>
+    </body>
+</html>
